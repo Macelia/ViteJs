1340189fac43691c6e277a581bbc32785ef38f6b
Macelia <massiliasaidoune06@gmail.com>
2024-01-23 21:28:25 +0100

    Quiz with Js

---
+++ main.js
@@ -0,0 +1,15 @@
+const app =document.querySelector("#app");
+
+const startbutton = document.querySelector("#Start");
+
+
+
+app.addEventListener("click", () => {
+    alert("Click App");
+},true);
+
+startbutton.addEventListener("click", () => {
+    alert("Click Start");
+}, true);
+
+// 46.36
